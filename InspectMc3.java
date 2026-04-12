c5963ac17956b932923703855135e80858015c4c
huangdihd <hd20100104@163.com>
2026-04-12 11:30:57 +0800

    Clean up temporary inspect and unused files

--- InspectMc3.java
+++ InspectMc3.java
@@ -1,23 +1,26 @@
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 
-public class InspectMc2 {
+public class InspectMc3 {
     public static void main(String[] args) throws Exception {
         URL[] urls = {new URL("file:///Users/h2028176/.m2/repository/org/geysermc/mcprotocollib/protocol/1.21-SNAPSHOT/protocol-1.21-SNAPSHOT.jar")};
         URLClassLoader cl = new URLClassLoader(urls);
         
         String[] classNames = {
-            "org.geysermc.mcprotocollib.protocol.packet.ingame.clientbound.inventory.ClientboundContainerSetContentPacket",
-            "org.geysermc.mcprotocollib.protocol.packet.ingame.clientbound.inventory.ClientboundContainerSetSlotPacket",
-            "org.geysermc.mcprotocollib.protocol.data.game.inventory.ContainerActionType",
-            "org.geysermc.mcprotocollib.protocol.data.game.inventory.ClickItemAction"
+            "org.geysermc.mcprotocollib.protocol.packet.ingame.clientbound.inventory.ClientboundOpenScreenPacket",
+            "org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.player.ServerboundInteractPacket",
+            "org.geysermc.mcprotocollib.protocol.data.game.entity.player.InteractAction"
         };
         
         for (String name : classNames) {
-            System.out.println("Methods in " + name + ":");
+            System.out.println("Constructors and Methods in " + name + ":");
             try {
                 Class<?> c = cl.loadClass(name);
+                for (Constructor<?> cons : c.getDeclaredConstructors()) {
+                    System.out.println("  " + cons.toString());
+                }
                 for (Method m : c.getDeclaredMethods()) {
                     System.out.println("  " + m.toString());
                 }
